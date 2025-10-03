6fe88a6550cb4360532968b925c8e6edef75962f
Jayesh-git10 <jayesh2005sharma@gmail.com>
2025-10-03 23:05:37 +0530

    Largest_array_element.cpp

--- Difficulty: Basic/Largest Element in array/Largest_array_element.cpp
+++ Difficulty: Basic/Largest Element in array/Largest_array_element.cpp
@@ -2,18 +2,21 @@
 #define ll long long int
 using namespace std;
 
-int largest_element_in_array(vector<int>& v, int n) {
-    int maxi = v[0];
-    for (int i = 0; i < n; i++) {
+ll largest_element_in_array(vector<ll>& v, ll n) {
+    ll maxi = v[0];
+    for (ll i = 0; i < n; i++) {
         if (maxi < v[i]) maxi = v[i];
     }
     return maxi;
 }
 
 int main() {
-    vector<ll> v = {1, 4, 3, 5};
-    int n = v.size();
-    int ans = largest_element_in_array(v, n);
+    ll n;
+    cin>>n;
+    vector<ll>v(n);
+    for(ll i=0;i<n;i++) cin>>v[i];
+    ll ans = largest_element_in_array(v, n);
     cout << ans << endl;
     return 0;
+
 }
